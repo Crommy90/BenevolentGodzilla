94dcdc2e77347ee67365ee11b899e3712c7ec9b5
Joshua Crompton <joshua.crompton@sigames.com>
2017-04-22 11:33:56 +0100

    Initial commit

---
+++ Source/BenevolentGodzilla/BenevolentGodzilla.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BenevolentGodzilla.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BenevolentGodzilla, "BenevolentGodzilla" );
---
+++ Source/BenevolentGodzilla/BenevolentGodzillaGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BenevolentGodzillaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BENEVOLENTGODZILLA_API ABenevolentGodzillaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
